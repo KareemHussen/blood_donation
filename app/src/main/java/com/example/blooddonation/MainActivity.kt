b6f6e60c710c1049acae9c21a3b34e87a5aa62dd
Kareen Hussen <kareemhussen500@gmai.com>
2022-12-14 22:44:39 +0200

    Let's Donate

--- app/src/main/java/com/example/blooddonation/MainActivity.kt
+++ app/src/main/java/com/example/blooddonation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.blooddonation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.blooddonation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
