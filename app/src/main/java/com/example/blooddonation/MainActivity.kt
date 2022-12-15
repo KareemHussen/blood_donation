de20ff713206df2be0e7977620b83844088f6ff0
‪Elsayed Mustafa‬‏ <110793510+El-Sayed-Mustafa@users.noreply.github.com>
2022-12-15 14:48:30 +0200

    Setup Navigation

--- app/src/main/java/com/example/blooddonation/MainActivity.kt
+++ app/src/main/java/com/example/blooddonation/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.blooddonation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.blooddonation.R
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+import com.example.blooddonation.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.bottomNavigationView.background = null;
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        val navController = findNavController(R.id.fragmentContainerView2)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
