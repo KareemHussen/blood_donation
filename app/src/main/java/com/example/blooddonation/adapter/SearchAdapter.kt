9a5efe7fcd7661fea07a5d6df3a3f385515218be
zyad <https://github.com/ZyadElfiky>
2022-12-16 22:15:08 +0200

    Blood Requests recycler view

---
+++ app/src/main/java/com/example/blooddonation/adapter/SearchAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.blooddonation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.blooddonation.databinding.ItemViewBinding
+
+
+class SearchAdapter
+    :ListAdapter<FakeClass,SearchAdapter.FakeViewHolder>(DiffCallback) {
+
+    class FakeViewHolder  private constructor(private val binding:ItemViewBinding)
+        :RecyclerView.ViewHolder(binding.root) {
+        fun bind(fakeClass: FakeClass) {
+
+            // bind xml views
+            //binding.fakeClass=FakeClass
+
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): FakeViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemViewBinding.inflate(layoutInflater, parent, false)
+                return FakeViewHolder(binding)
+            }
+        }
+    }
+
+    companion object DiffCallback:DiffUtil.ItemCallback<FakeClass>() {
+        override fun areItemsTheSame(oldItem: FakeClass, newItem: FakeClass): Boolean {
+            return oldItem==newItem
+        }
+
+        override fun areContentsTheSame(oldItem: FakeClass, newItem: FakeClass): Boolean {
+            return oldItem.id==newItem.id
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FakeViewHolder {
+       return FakeViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: FakeViewHolder, position: Int) {
+        val fakeClass=getItem(position)
+        holder.bind(fakeClass)
+    }
+
+}
+
+data class FakeClass (val id:String)
+
