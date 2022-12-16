ba5168e4d3a3325107038ec4f438dd59144e45b6
Hadi Ehab <hadiehab17@gmail.com>
2022-12-16 20:59:53 +0200

    Finished onboarding and splash screen
    Still the splash screen requires text

---
+++ app/src/main/java/com/example/blooddonation/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.example.blooddonation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.blooddonation.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var loginBinding:FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        loginBinding= DataBindingUtil.inflate(inflater,R.layout.fragment_login, container, false)
+        val text = "<font color=#FF2156>Dare</font> <font color=#595959>To</font> <font color=#FF2156>Donate</font>"
+        loginBinding.donateText.text = HtmlCompat.fromHtml(text,HtmlCompat.FROM_HTML_OPTION_USE_CSS_COLORS)
+        return loginBinding.root
+    }
+
+}
--- app/src/main/java/com/example/blooddonation/MainActivity.kt
+++ app/src/main/java/com/example/blooddonation/MainActivity.kt
@@ -1,30 +1,66 @@
 package com.example.blooddonation
 
+import android.content.Context
 import android.os.Bundle
-import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
-
 import com.example.blooddonation.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_BloodDonation)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.bottomNavigationView.background = null;
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
 
         val navController = findNavController(R.id.fragmentContainerView2)
-        binding.bottomNavigationView.setupWithNavController(navController)
 
+
+        val graphInflater = navController.navInflater
+        val navGraph = graphInflater.inflate(R.navigation.nav_graph)
+        val destination=if(onBoardingFinished()) R.id.loginFragment else R.id.viewPagerFragment
+
+        navGraph.setStartDestination(destination)
+        navController.graph=navGraph
+        getMainActivityComponentsState(navController)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        supportActionBar?.hide()
     }
+
+    private fun getMainActivityComponentsState(navController: NavController) {
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.profileFragment,
+                R.id.searchFragment,
+                R.id.donationRequestFragment
+            )
+        )
+        navController.addOnDestinationChangedListener { _: NavController, destination: NavDestination, _: Bundle? ->
+            binding.bottomAppBar.isVisible =
+                appBarConfiguration.topLevelDestinations.contains(destination.id)
+            binding.fab.isVisible=appBarConfiguration.topLevelDestinations.contains(destination.id)
+        }
+    }
+    private fun onBoardingFinished(): Boolean{
+        val sharedPref = getSharedPreferences("onBoarding", MODE_PRIVATE)
+        return sharedPref.getBoolean("Finished", false)
+    }
+
+
 }
