f2d8066c41b88a6b3b2dfa73f6d395fe23a323c5
yusufnasserdev <yusufnassereng@gmail.com>
2022-12-21 22:39:35 +0200

    Implemented layout inflation for info_box

--- app/src/main/java/com/example/blooddonation/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/blooddonation/profile/ProfileFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.core.view.marginStart
 import androidx.databinding.DataBindingUtil
 import com.example.blooddonation.R
 import com.example.blooddonation.databinding.FragmentProfileBinding
+import com.example.blooddonation.databinding.InfoBlockBinding
+import kotlin.math.absoluteValue
 
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
     private lateinit var viewModel: ProfileViewModel
+    private val dummyInfoList: List<Pair<String, String>>
+        get() {
+            return listOf(
+                Pair("A+" , "Blood Type"),
+                Pair("05" , "Donated"),
+                Pair("02" , "Requested"),
+            )
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +34,18 @@ class ProfileFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val linearLayout = binding.userInfoLayout
+        for (block in dummyInfoList) {
+            val blockBinding : InfoBlockBinding = DataBindingUtil.inflate(layoutInflater, R.layout.info_block, null, false)
+
+            blockBinding.infoText.text = block.first
+            blockBinding.infoTypeText.text = block.second
+
+            linearLayout.addView(blockBinding.root)
+        }
     }
 
 }
